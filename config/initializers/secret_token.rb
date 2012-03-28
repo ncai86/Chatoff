9865387b633bbc590597952e39148c9b2a0c8931
ncai86 <nicholascai86@gmail.com>
2012-03-28 16:21:55 +0800

    adding existing files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chatoff::Application.config.secret_token = '4f296f19c72ce3cc4c45bf8ab59dcfa6e162d6211dd15f69b4f0a374710bcedc51d8f0b2138489b4a0812cde1b5998a7e79ac34c52fa3db919b811bd2d6a964e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Chatoff::Application.config.session_store :cookie_store, key: '_chatoff_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Chatoff::Application.config.session_store :active_record_store
