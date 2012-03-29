81c99aeb43f9f94b8a10164af1b5c2b04a57e34f
ncai86 <nicholascai86@gmail.com>
2012-03-30 01:35:50 +0800

    added styling

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,9 @@
 class MessagesController < ApplicationController
+  before_filter :authenticate_user!
+
   def index
     @messages = Message.all
+    @user_id = session["warden.user.user.key"][1][0]
   end
 
   def create
